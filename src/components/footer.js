40c738334738066a81cb116b27863ec8b5e497ad
Carlos84sh <Cs.huelves@gmail.com>
2021-01-01 19:52:44 +0000

    revision

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ export default function(props) {
         <footer className="footer">
             <div className="container">
                 <p>&copy; 2020. Carlos Sánchez Huelves</p>
+                <span>Estado: {store.loading ? "Cargando...":"Cargado"}</span> 
             </div>
         </footer>
     )
